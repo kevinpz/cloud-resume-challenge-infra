dae932ea8d3d013446865292dfd4626bf169621a
Kevin <printz.kevin@gmail.com>
2020-07-20 18:19:31 -0400

    fix image

--- rwerwe/check_python.sh
+++
@@ -1,6 +0,0 @@
-pwd
-ls
-cat scripts/check_python.sh
-pip install pylint
-pip install -r visitor_count/requirements.txt
-pylint visitor_count/
--- rwerwe/send_python.sh
+++
@@ -1,3 +0,0 @@
-apt install -y zip
-zip -r visitor_count.zip sources/visitor_count
-gsutil cp visitor_count.zip gs://coffeetime-dev-cloudresume-functions
